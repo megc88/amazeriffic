ea236216fe98fea24bdbff78c3609bd6d7702021
Megan Conlin <Megan Conlin>
2016-10-18 21:19:47 -0700

    create forEach loop

--- javascripts/app.js
+++ javascripts/app.js
@@ -1,17 +1,14 @@
 var main = function() {
   "use strict";
 
-var tabNumber;
-
-for (tabNumber = 1; tabNumber <=3; tabNumber++) {
-  var tabSelector = ".tabs a:nth-child(" + tabNumber + ") span";
-  $(tabSelector).on("click", function(event) {
+$(".tabs span").toArray().forEach(function (element) {
+  $(element).on("click", function() {
     $(".tabs span").removeClass("active");
-    $(event.target).addClass("active");
+    $(element).addClass("active");
     $("main .content").empty();
     return false;
   });
-  }
+ });
 };
 
 $(document).ready(main);
