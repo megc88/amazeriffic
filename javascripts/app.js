3c9bc4b504c8705d7f14144a73b8e36b5827e287
Megan Conlin <Megan Conlin>
2016-10-18 21:11:45 -0700

    turning into for loop

--- javascripts/app.js
+++ javascripts/app.js
@@ -1,30 +1,18 @@
 var main = function() {
   "use strict";
 
+var tabNumber;
 
-var makeTabActive = function (tabNumber) {
+for (tabNumber = 1; tabNumber <=3; tabNumber++) {
   var tabSelector = ".tabs a:nth-child(" + tabNumber + ") span";
-  $(".tabs span").removeClass("active");
-  $(tabSelector).addClass("active");
-  $("main .content").empty();
- };
-
- $(".tabs a:nth-child(1)").on("click", function () {
-   makeTabActive(1);
-   return false;
- });
-
- $(".tabs a:nth-child(2)").on("click", function() {
-   makeTabActive(2);
-   return false;
- });
-
- $(".tabs a:nth-child(3)").on("click", function() {
-   makeTabActive(3);
-   return false;
- });
-
- };
+  $(tabSelector).on("click", function(event) {
+    $(".tabs span").removeClass("active");
+    $(event.target).addClass("active");
+    $("main .content").empty();
+    return false;
+  });
+  }
+};
 
 $(document).ready(main);
 
